062d858d383870736f4f048771af298692981456
sham-shlke <shamshelke540@gmail.com>
2024-04-15 08:39:49 +0530

    EDA And Problem statement

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -35,6 +35,7 @@ class DataIngestion:
             train_set.to_csv(self.ingestion_config.train_data_path,index=False,header=True)
             test_set.to_csv(self.ingestion_config.test_data_path,index=False,header=True)
             logging.info("Ingestion of the data in completed")
+            
             return(
                 self.ingestion_config.train_data_path,
                 self.ingestion_config.test_data_path
